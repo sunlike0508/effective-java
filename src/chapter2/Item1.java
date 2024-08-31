24fed63ead5fca76cd43754b10119bc9d5338813
seonhoshin <sunlike0508@waug.com>
2024-08-31 23:31:19 +0900

    초기화

--- src/chapter2/Item1.java
+++ src/chapter2/Item1.java
@@ -1,2 +1,11 @@
-package chapter2;public class Item1 {
-}
+package chapter2;
+
+/**
+ *  생성자 대신 정적 팩터리 메서드를 고려하라.
+ *  <p></p>
+ *  * 디자인 패턴의 팩터리 메서드랑 다르다.
+ *  <p></p>
+ *  
+ */
+
+public class Item1 {}
